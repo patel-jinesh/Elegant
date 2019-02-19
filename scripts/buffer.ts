40804b3193c63ee1da612bf6e82c29f6264269ab
Jinesh Patel <programmer.jinesh@gmail.com>
2019-02-19 05:08:12 -0500

    added algorithmic comments to buffer

--- scripts/buffer.ts
+++ scripts/buffer.ts
@@ -3,45 +3,77 @@ export class Buffer {
     private cursor: number;
 
     public constructor(data: string) {
+        // The cursor should be at the start of the file.
         this.cursor = 0;
         this.data = data;
     }
 
     public seek(regex: RegExp): number {
-        regex = new RegExp(regex, "g");
-        regex.lastIndex = this.cursor;
-            
-        let m = regex.exec(this.data);
-
-        if (m == null || m.index != this.cursor)
-            return 0;
-        
-        this.cursor += m[0].length;
-        return m[0].length;
-    }
-
-    public capture<T>(regex: RegExp, type: new (string: string) => T): T[] {
+        /*
+            The regex requires the global flag to make sure it uses the lastIndex property
+            so that it matches starting at the cursor location. Thus we add the global flag
+            if it is not already included while maintaining the previous flags.
+        */
         let flags = regex.flags;
 
         if (!flags.includes("g"))
             flags += "g";
         
         regex = new RegExp(regex, flags);
+
+        // Set the lastIndex to the cursor as the starting position for matching.
         regex.lastIndex = this.cursor;
 
+        // Execute the regex and find a match.
         let m = regex.exec(this.data);
 
+        // If no match was found or the match was not at the current cursor location return 0.
+        if (m == null || m.index != this.cursor)
+            return 0;
+
+        // Move the cursor to the end of the catured value.
+        this.cursor += m[0].length;
+
+        // Returns the number of positions the cursor moved.
+        return m[0].length;
+    }
+
+    public capture<T>(regex: RegExp, type: new (string: string) => T): T[] {
+        /*
+            The regex requires the global flag to make sure it uses the lastIndex property
+            so that it matches starting at the cursor location. Thus we add the global flag
+            if it is not already included while maintaining the previous flags.
+        */
+        let flags = regex.flags;
+
+        if (!flags.includes("g"))
+            flags += "g";
+        
+        regex = new RegExp(regex, flags);
+
+        // Set the lastIndex to the cursor as the starting position for matching.
+        regex.lastIndex = this.cursor;
+
+        // Execute the regex and find a match.
+        let m = regex.exec(this.data);
+
+        // If no match was found or the match was not at the current cursor location return null.
         if (m == null || m.index != this.cursor)
             return null;
 
+        // Move the cursor to the end of the catured value.
         this.cursor += m[0].length;
 
+        // If there are no groups, return only the whole match.
         if (m.length == 1)
             return [new type(m[0])];
 
+        // Otherwise extract the groups and return all the captured groups.
         let vals : T[] = [];
 
+        // First value is the whole matched regex, so it is discarded.
         for (let i = 1; i < m.length; i++)
+            // Don't return undefined groups.
             if (m[i] != undefined)
                 vals.push(new type(m[i]));
         
